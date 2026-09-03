fe7861fb31436a02da908a05b0f6203f2f069be7
ishaposs <ishavp05@gmail.com>
2026-09-03 16:51:16 +0530

    Fix department assignment, SLA, and ticket UI bugs

--- Backend/SCIQUSTICKETS.BUSINESS/BusinessModels/RequestDTOs/DepartmentRequestDTOS/UpdateDepartmentRequest.cs
+++ Backend/SCIQUSTICKETS.BUSINESS/BusinessModels/RequestDTOs/DepartmentRequestDTOS/UpdateDepartmentRequest.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SCIQUSTICKETS.BUSINESS.BusinessModels.RequestDTOs
 {
 	public class UpdateDepartmentRequest
 	{
 		public string? Name { get; set; }
+
+		public string? TicketAutoAssignMethod { get; set; }
+		public double? W_Load { get; set; }
+		public double? W_Severity { get; set; }
+		public double? W_Recency { get; set; }
 	}
 }
